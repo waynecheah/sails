9cb0ca7b897f3072b22b0cfd256e4a599a6fded2
Wayne Cheah <cheahkokweng@gmail.com>
2013-05-24 17:50:05 +0800

    added RequireJS optimizer (use for mobile app)

---
+++ build/build.js
@@ -0,0 +1,11 @@
+({
+	baseUrl: '../assets/js',
+	mainConfigFile: '../assets/main.js',
+	name: '../assets/main',
+	out: '../assets/app.min.js',
+	preserveLicenseComments: false,
+	paths: {
+		requireLib: 'libs/require'
+	},
+	include: 'requireLib'
+})
